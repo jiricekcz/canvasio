87fe22729e43394befeccc7b081e90af53584ed3
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2020-12-26 20:22:55 +0100

    old geometry remove

--- src/geometry/constants.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description Amount of decimal places round coordinates to
- */
-export const decimalRoundCoordinate = 6;
-/**
- * @description Amount of decimal places round angles in radians to
- */
-export const decimalRoundAngle = 15;
--- src/geometry/index.js
+++
@@ -1,8 +0,0 @@
-// File used to unify all exports
-import { Point } from './objects/Point.js';
-import { Line } from './objects/Line.js';
-import { Polynom } from './objects/Polynom.js';
-
-export {
-    Point, Line, Polynom
-}
