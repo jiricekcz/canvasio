4ad2f304cc90aa4ec7cbfd01ff5babfba95d4f31
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-01-04 23:21:34 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -749,7 +749,7 @@ class FilterManager extends Array {
     }
 }
 
-var Filter = {};
+export var Filter = {};
 /**
  * @description Represents a filter that can be applied to the canvas
  * @class
@@ -985,7 +985,7 @@ Filter.Sepia = class SepiaFilter extends Filter.Base {
  * @param {"coordinate" | "angle"}
  * @returns {Number}
  */
-function round(x, type = "coordinate") {
+export function round(x, type = "coordinate") {
     switch (type) {
         case "coordinate": return Math.round(x * Math.pow(10, decimalRoundCoordinate)) / Math.pow(10, decimalRoundCoordinate);
         case "angle": return Math.round(x * Math.pow(10, decimalRoundAngle)) / Math.pow(10, decimalRoundAngle);
